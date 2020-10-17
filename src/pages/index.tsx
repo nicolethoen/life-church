ea4638049d6b2ac651a6335a550e207cc70776c1
nicolethoen <nthoen@redhat.com>
2020-10-17 12:07:48 -0400

    fix live stream link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,12 +24,14 @@ export default () => {
         <div className="lc-welcome-message">
             {smallVideo ? (
                 <iframe
-                    src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FLifeChurchAyer%2Fposts%2F157283136064182&width=480"
-                    width="480" height="500" style={{border:"none",overflow:"hidden"}} scrolling="no" frameBorder="0" />
+                    src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FLifeChurchAyer%2Fposts%2F157283136064182&show_text=true&width=480&appId=612285882769039&height=498"
+                    width="480" allow="encrypted-media"
+                    height="500" style={{border:"none",overflow:"hidden"}} />
             ) : (
                 <iframe
-                    src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FLifeChurchAyer%2Fposts%2F157283136064182&width=640"
-                    width="640" height="500" style={{border:"none",overflow:"hidden"}} scrolling="no" frameBorder="0"/>
+                    src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FLifeChurchAyer%2Fposts%2F157283136064182&show_text=true&width=552&appId=612285882769039&height=498"
+                    width="552" allow="encrypted-media"
+                    height="500" style={{border:"none",overflow:"hidden"}} />
             )}
         </div>
     )
