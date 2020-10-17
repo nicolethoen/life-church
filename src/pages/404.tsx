715568a054320c09788235793d6c7ba1b13747f6
nicolethoen <nthoen@redhat.com>
2020-10-17 19:16:53 -0400

    why is order of service broken again

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default () => (
   <div>
-    <h1>404 - Oh no's! We couldn't find that page :(</h1>
+      <h1>Something went wrong... </h1>
+      <p>If you keep seeing this message, head to <a href="https://www.facebook.com/LifeChurchAyer/">our Facebook page</a> for updates.</p>
   </div>
 )
