77eb3f904659d6a1f030d5ef15772205613b5664
nicolethoen <nthoen@redhat.com>
2020-10-25 10:15:26 -0400

    update livestream link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,12 +24,12 @@ export default () => {
         <div className="lc-welcome-message">
             {smallVideo ? (
                 <iframe
-                    src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FLifeChurchAyer%2Fposts%2F159570289168800&show_text=true&width=552&appId=612285882769039&height=536"
+                    src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FLifeChurchAyer%2Fposts%2F159893109136518&show_text=true&width=552&appId=612285882769039&height=561"
                     width="400" allow="encrypted-media"
                     height="500" style={{border:"none",overflow:"hidden"}} />
             ) : (
                 <iframe
-                    src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FLifeChurchAyer%2Fposts%2F159570289168800&show_text=true&width=552&appId=612285882769039&height=536"
+                    src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FLifeChurchAyer%2Fposts%2F159893109136518&show_text=true&width=552&appId=612285882769039&height=561"
                     width="552" height="536" style={{border:"none",overflow:"hidden"}} scrolling="no" frameBorder="0"
                     allow="encrypted-media" />
             )}
